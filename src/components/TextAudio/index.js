29b35f9e300d26ad9122c535fdec173442f06994
arkanttus <italo.oliveira300@hotmail.com>
2020-07-08 21:21:29 -0500

    update: Elements in Page Audio

--- src/components/TextAudio/index.js
+++ src/components/TextAudio/index.js
@@ -3,11 +3,26 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import { FaRegPlayCircle } from "react-icons/fa";
+import './TextAudio.css'
 
-export default function Gallery() {
+export default function Gallery(props) {
+  const Page = props.content.Img
+
   return (
     <Container>
-      <Row> AUDIOOOOO</Row>
+      <Row> 
+        <Col className='display-center'>
+          <Page className='page-audio'/>
+        </Col>
+      </Row>
+      <Row>
+        <Col className='display-center'>
+          <Button className='btn-listen'>
+            <FaRegPlayCircle className='icon-listen'/> Ouvir
+          </Button>
+        </Col>
+      </Row>
     </Container>
   );
 }
